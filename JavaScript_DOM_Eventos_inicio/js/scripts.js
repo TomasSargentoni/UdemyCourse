5a28194adf13a2bf1d981bc5e7d368452dc1e572
Tomas <tomassargentoni92@hotmail.com>
2023-12-23 14:44:44 -0300

    agregando algunas cositas mas... seguir viendo!

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -7,7 +7,26 @@ console.log(heading);
 const enlaces = document.querySelectorAll(".navegacion a");
 enlaces[0].textContent = "Nuevo texto para enlace";
 enlaces[0].classList.add("nueva-clase");
-enlaces[0].classList.remove("navegacion__enlace");
-console.log(enlaces[0])
+// enlaces[0].classList.remove("navegacion__enlace");
 
 // getElementById NO SE USA
+const nuevoEnlace = document.createElement("A")
+
+// agregar el href
+nuevoEnlace.href = "nuevo-enlace.html";
+
+
+// agregar el texto
+nuevoEnlace.textContent = "Un nuevo Enlace";
+
+
+// agregar la clase
+nuevoEnlace.classList.add("navegacion__enlace")
+
+
+// agregarlo al Documento
+
+const navegacion = document.querySelector(".navegacion")
+navegacion.appendChild(nuevoEnlace)
+
+console.log(nuevoEnlace);
