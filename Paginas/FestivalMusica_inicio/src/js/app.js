28aaadbfb61e7178ca523ef61ea4a3d86cbceb1c
Duilio Tomas Sargentoni Audisio <89033@sistemas.frc.utn.edu.ar>
2024-01-04 20:50:31 -0300

    agregando mucho js al script.. navegacion + galery

--- Paginas/FestivalMusica_inicio/src/js/app.js
+++ Paginas/FestivalMusica_inicio/src/js/app.js
@@ -3,7 +3,28 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 function iniciarApp() {
+    
     crearGaleria();
+    scrollNav();
+}
+
+
+
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll(".navegacion-principal a")
+
+
+    enlaces.forEach(enlace => {
+        enlace.addEventListener("click", function(e) {
+            e.preventDefault();
+            
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth" });
+
+        })
+    });
 }
 
 
@@ -18,8 +39,45 @@ function crearGaleria() {
             <img loading="lazy" width="200" height="300" src="build/img/thumb/
             ${i}.jpg" alt="imagen galeria">
         `
-
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
 
         galeria.appendChild(imagen)
     }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement("picture")
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/
+        ${id}.jpg" alt="imagen galeria">
+    `;
+
+    // Crea el overlay con la imagen
+    const overlay = document.createElement("DIV");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay");
+    overlay.onclick = function() {
+        overlay.remove();
+        body.classList.remove("fijar-body")
+    }
+
+    // Boton para cerrar el Modal
+
+    const cerrarModal = document.createElement("P")
+    cerrarModal.textContent = "X";
+    cerrarModal.classList.add("btn-cerrar");
+    cerrarModal.onclick = function() {
+        overlay.remove();
+        body.classList.remove("fijar-body")
+    }
+    overlay.appendChild(cerrarModal)
+
+    // Añadirlo al html
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add("fijar-body")
 }
