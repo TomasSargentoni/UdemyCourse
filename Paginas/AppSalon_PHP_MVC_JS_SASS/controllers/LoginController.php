79132bc3d8f8110d10136499ee190345d4d050dd
Tomas <tomassargentoni92@hotmail.com>
2024-02-23 18:41:09 -0300

    avanzando

--- Paginas/AppSalon_PHP_MVC_JS_SASS/controllers/LoginController.php
+++ Paginas/AppSalon_PHP_MVC_JS_SASS/controllers/LoginController.php
@@ -10,6 +10,8 @@ class LoginController {
     public static function login(Router $router) {
         
         
+
+
         $router->render("auth/login");
     }
 
@@ -77,5 +79,32 @@ class LoginController {
     public static function mensaje(Router $router) {
 
         $router->render("auth/mensaje");
+    }
+
+    public static function confirmar(Router $router) {
+        $alertas = [];
+
+        $token = s($_GET["token"]);
+
+        $usuario = Usuario::where("token", $token);
+
+        if (empty($usuario)) {
+            // Mostrar mensaje de error
+            Usuario::setAlerta("error", "Token No Valido");
+        } else {
+            // Modificar a usuario confirmado
+            $usuario->confirmado = "1";
+            $usuario->token = null;
+            $usuario->guardar();
+            $usuario::setAlerta("exito", "Cuenta Comprobada Correctamente");
+        }
+
+        // Obtener alertas
+        $alertas = Usuario::getAlertas();
+        
+        // Renderizar la vista
+        $router->render("auth/confirmar-cuenta", [
+            "alertas" => $alertas
+        ]);
     }
 }
