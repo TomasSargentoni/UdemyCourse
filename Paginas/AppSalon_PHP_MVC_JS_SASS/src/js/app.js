6a3eb11df21d67e4c243bbc59510118774802d8a
Tomas <tomassargentoni92@hotmail.com>
2024-03-01 13:04:41 -0300

    agregando JS

---
+++ Paginas/AppSalon_PHP_MVC_JS_SASS/src/js/app.js
@@ -0,0 +1,41 @@
+let paso=1;
+
+document.addEventListener("DOMContentLoaded", function() {
+    iniciarApp();
+});
+
+
+function iniciarApp() {
+    mostrarSeccion(); // Muestra y oculta las secciones
+    tabs(); // Cambia la seccion cuando se presionen los tabs
+
+}
+
+function mostrarSeccion() {
+    
+    // Ocultar la seccion que tenga la clase de mostrar
+    const seccionAnterior = document.querySelector(".mostrar")
+    if(seccionAnterior) {
+        seccionAnterior.classList.remove("mostrar")
+    }
+
+
+    // Seleccionar la seccion con el paso...
+    const pasoSelector = `#paso-${paso}`
+    const seccion = document.querySelector(pasoSelector)
+    seccion.classList.add("mostrar");
+}
+
+function tabs() {
+    const botones = document.querySelectorAll(".tabs button")
+
+    botones.forEach( boton => {
+        boton.addEventListener("click", function(e) {
+            paso = parseInt( e.target.dataset.paso );
+
+            mostrarSeccion();
+        });
+    })
+
+
+}
