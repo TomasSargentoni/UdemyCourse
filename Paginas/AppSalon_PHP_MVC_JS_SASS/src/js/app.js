0c11d72f762e81e56e69c35cecdc1411e22e1835
Tomas <tomassargentoni92@hotmail.com>
2024-03-04 17:54:26 -0300

    Avanzando

--- Paginas/AppSalon_PHP_MVC_JS_SASS/src/js/app.js
+++ Paginas/AppSalon_PHP_MVC_JS_SASS/src/js/app.js
@@ -1,4 +1,13 @@
 let paso=1;
+const pasoInicial = 1;
+const pasoFinal = 3;
+
+const cita = {
+    nombre: "",
+    fecha: "",
+    hora: "",
+    servicios: [] 
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     iniciarApp();
@@ -8,6 +17,11 @@ document.addEventListener("DOMContentLoaded", function() {
 function iniciarApp() {
     mostrarSeccion(); // Muestra y oculta las secciones
     tabs(); // Cambia la seccion cuando se presionen los tabs
+    botonesPaginador(); // Agrega o quita los botones del paginador
+    paginaSiguiente(); 
+    paginaAnterior(); 
+
+    consultarAPI(); // Consulta la API en el backend de PHP
 
 }
 
@@ -24,6 +38,16 @@ function mostrarSeccion() {
     const pasoSelector = `#paso-${paso}`
     const seccion = document.querySelector(pasoSelector)
     seccion.classList.add("mostrar");
+
+    // Quita la clase de actual al tab anterior
+    const tabAnterior = document.querySelector(".actual");
+    if (tabAnterior) {
+        tabAnterior.classList.remove("actual");
+    }
+
+    // Resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`)
+    tab.classList.add("actual");
 }
 
 function tabs() {
@@ -34,8 +58,114 @@ function tabs() {
             paso = parseInt( e.target.dataset.paso );
 
             mostrarSeccion();
+
+            botonesPaginador();
         });
     })
+}
 
 
+function botonesPaginador() {
+    const paginaAnterior = document.querySelector("#anterior");
+    const paginaSiguiente = document.querySelector("#siguiente");
+
+    console.log(paginaAnterior);
+
+    if (paso === 1) {
+        paginaAnterior.classList.add("ocultar");
+        paginaSiguiente.classList.remove("ocultar");
+    } else if (paso === 3) {
+        paginaAnterior.classList.remove("ocultar");
+        paginaSiguiente.classList.add("ocultar");
+    } else {
+        paginaAnterior.classList.remove("ocultar");
+        paginaSiguiente.classList.remove("ocultar");
+    }
+
+    mostrarSeccion();
+}
+
+function paginaAnterior() {
+    const paginaAnterior = document.querySelector("#anterior");
+    paginaAnterior.addEventListener("click", function() {
+        
+        if(paso <= pasoInicial) return;
+        paso--;
+
+        botonesPaginador();
+    })
+}
+
+function paginaSiguiente() {
+    const paginaSiguiente = document.querySelector("#siguiente");
+    paginaSiguiente.addEventListener("click", function() {
+        
+        if(paso >= pasoFinal) return;
+        paso++;
+
+        botonesPaginador();
+    })
+}
+
+async function consultarAPI() {
+
+    try {
+        const url = "http://localhost:3000/api/servicios"
+        const resultado = await fetch(url);
+        const servicios = await resultado.json();
+        mostrarServicios(servicios);
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function mostrarServicios(servicios) {
+    servicios.forEach( servicio => {
+        const { id, nombre, precio} = servicio;
+        
+        const nombreServicio = document.createElement("P");
+        nombreServicio.classList.add("nombre-servicio");
+        nombreServicio.textContent = nombre;
+
+        const precioServicio = document.createElement("P");
+        precioServicio.classList.add("precio-servicio");
+        precioServicio.textContent = `$${precio}`;
+
+        const servicioDiv = document.createElement("DIV");
+        servicioDiv.classList.add("servicio");
+        servicioDiv.dataset.idServicio = id;
+        servicioDiv.onclick = function() {
+            seleccionarServicio(servicio);
+        };
+
+        servicioDiv.appendChild(nombreServicio);
+        servicioDiv.appendChild(precioServicio);
+
+        document.querySelector("#servicios").appendChild(servicioDiv);
+    });
+}
+
+function seleccionarServicio(servicio) {
+    const { id } = servicio;
+    const { servicios } = cita;
+
+    // Identificar el elemento al que se le da click
+    const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);
+
+    // Comprobar si un servicio ya fue agregado 
+    if( servicios.some( agregado => agregado.id === id ) ) {
+        // Eliminarlo
+        cita.servicios = servicios.filter( agregado => agregado.id !== id);
+        divServicio.classList.remove("seleccionado");
+    } else {
+        // Agregarlo
+        cita.servicios = [...servicios, servicio];
+        divServicio.classList.add("seleccionado");
+    }
+
+    
+    
 }
