9603bc72f126f4ba7419dd009fe0f941dee2d533
Tomas <tomassargentoni92@hotmail.com>
2024-03-11 20:19:36 -0300

    vistas realizadas

--- Paginas/UpTask_MVC/controllers/LoginController.php
+++ Paginas/UpTask_MVC/controllers/LoginController.php
@@ -34,29 +34,45 @@ class LoginController {
         ]);
     }
 
-    public static function olvide() {
-        echo "Desde Olvide";
+    public static function olvide(Router $router) {
+
 
         if($_SERVER["REQUEST_METHOD"] === "POST") {
 
         }
+
+        // Render la vista
+        $router->render("auth/olvide", [
+            "titulo" => "Olvide mi Password"
+        ]);
     }
 
-    public static function reestablecer() {
-        echo "Desde Reestablecer";
+    public static function reestablecer(Router $router) {
 
         if($_SERVER["REQUEST_METHOD"] === "POST") {
 
         }
+
+        // Render la vista
+        $router->render("auth/reestablecer", [
+            "titulo" => "Reestablecer Password"
+        ]);
     }
 
-    public static function mensaje() {
-        echo "Desde Mensaje";
+    public static function mensaje(Router $router) {
+        
+        // Render la vista
+        $router->render("auth/mensaje", [
+            "titulo" => "Cuenta Creada Exitosamente"
+        ]);
 
     }
 
-    public static function confirmar() {
-        echo "Desde Confirmar";
-
+    public static function confirmar(Router $router) {
+                
+        // Render la vista
+        $router->render("auth/confirmar", [
+            "titulo" => "Confirma tu cuenta UpTask"
+        ]);
     }
 }
--- Paginas/UpTask_MVC/views/auth/reestablecer.php
+++ Paginas/UpTask_MVC/views/auth/reestablecer.php
@@ -1,20 +1,16 @@
-<div class="contenedor login">
+<div class="contenedor reestablecer">
     <?php include_once __DIR__ . "/../templates/nombre-sitio.php"; ?>
     <div class="contenedor-sm">
-        <p class="descripcion-pagina">Iniciar Sesion</p>
+        <p class="descripcion-pagina">Coloca tu nuevo password</p>
 
-        <form class="formulario" method="POST" action="/">
-            <div class="campo">
-                <label for="email">Email</label>
-                <input type="email" id="email" placeholder="Tu Email" name="email">
-            </div>
+        <form class="formulario" method="POST" action="/reestablecer">
 
             <div class="campo">
                 <label for="password">Password</label>
                 <input type="password" id="password" placeholder="Tu Password" name="password">
             </div>
 
-            <input type="submit" class="boton" value="Iniciar Sesion">
+            <input type="submit" class="boton" value="Guardar Password">
         </form>
 
         <div class="acciones">
