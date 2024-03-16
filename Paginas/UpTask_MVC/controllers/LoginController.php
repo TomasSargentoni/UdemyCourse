c22280d7d3131b68970551dc5be0510955291c90
Tomas <tomassargentoni92@hotmail.com>
2024-03-16 16:40:40 -0300

    actualizando

--- Paginas/UpTask_MVC/controllers/LoginController.php
+++ Paginas/UpTask_MVC/controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Usuario;
 
 class LoginController {
     public static function login(Router $router) {
@@ -24,13 +25,41 @@ class LoginController {
     
     public static function crear(Router $router) {
 
-        if($_SERVER["REQUEST_METHOD"] === "POST") {
+        $alertas = [];
+        $usuario = new Usuario;
 
+        if($_SERVER["REQUEST_METHOD"] === "POST") {
+            $usuario->sincronizar($_POST);
+            $alertas = $usuario->validarNuevaCuenta();
+            
+            if(empty($alertas)) {
+                $existeUsuario = Usuario::where("email", $usuario->email);
+
+                if($existeUsuario) {
+                    Usuario::setAlerta("error", "El Usuario ya esta registrado");
+                    $alertas = Usuario::getAlertas();
+                } else {
+                    // Hashear el password
+                    $usuario->hashPassword();
+
+                    // Eliminar password2
+                    unset($usuario->password2);
+                    
+                    // Generar el Token
+                    $usuario->crearToken(); 
+
+                    // Crear un nuevo usuario
+                }
+            }
+
+            debuguear($usuario);
         }
 
         // Render la vista
         $router->render("auth/crear", [
-            "titulo" => "Crea tu cuenta en UpTask"
+            "titulo" => "Crea tu cuenta en UpTask",
+            "usuario" => $usuario,
+            "alertas" => $alertas
         ]);
     }
 
