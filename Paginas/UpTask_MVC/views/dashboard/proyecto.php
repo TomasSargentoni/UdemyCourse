be59de447aecab2eb4acf9c47ccc5ac373842b51
Duilio Tomas Sargentoni Audisio <89033@sistemas.frc.utn.edu.ar>
2024-03-26 18:24:26 -0300

    Agregando Menu Mobile

--- Paginas/UpTask_MVC/views/dashboard/proyecto.php
+++ Paginas/UpTask_MVC/views/dashboard/proyecto.php
@@ -6,6 +6,24 @@
                 &#43;Nueva Tarea
             </button>
         </div>
+    
+        <div id="filtros" class="filtros">
+            <div class="filtros-inputs">
+                <h2>Filtros</h2>
+                <div class="campo">
+                    <label for="todas">Todas</label>
+                    <input type="radio" id="todas" name="filtro" value="" checked>
+                </div>
+                <div class="campo">
+                    <label for="completadas">Completadas</label>
+                    <input type="radio" id="completadas" name="filtro" value="1">
+                </div>
+                <div class="campo">
+                    <label for="pendientes">Pendientes</label>
+                    <input type="radio" id="pendientes" name="filtro" value="0">
+                </div>
+            </div>
+        </div>
 
         <ul id="listado-tareas" class="listado-tareas"></ul>
     </div>
@@ -14,7 +32,7 @@
 
 <?php
 
-$script = '
+$script .= '
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="build/js/tareas.js"></script>   
 ';
