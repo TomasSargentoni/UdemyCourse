927cb2b3df7da5989a7e39f7be814e72c56798cb
Tomas <tomassargentoni92@hotmail.com>
2024-03-28 17:32:27 -0300

    Creando Proyecto Nuevo

--- Paginas/DevWebCamp_inicio/includes/database.php
+++ Paginas/DevWebCamp_inicio/includes/database.php
@@ -1,10 +1,12 @@
 <?php
+
 $db = mysqli_connect(
     $_ENV['DB_HOST'] ?? '',
     $_ENV['DB_USER'] ?? '', 
     $_ENV['DB_PASS'] ?? '', 
     $_ENV['DB_NAME'] ?? ''
 );
+
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
---
+++ Paginas/DevWebCamp_inicio/views/auth/login.php
@@ -0,0 +1,23 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Inicia sesion en DevWebcamp</p>
+
+    <form class="formulario">
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input type="email" class="formulario__input" placeholder="Tu Email" id="email" name="email">
+        </div>
+
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Password</label>
+            <input type="email" class="formulario__input" placeholder="Tu Password" id="password" name="password">
+        </div>
+
+        <input type="submit" class="formulario__submit" value="Iniciar Sesion">
+    </form>
+
+    <div class="acciones">
+        <a href="/registro" class="acciones__enlace">¿Aun no tienes una cuenta? Obtener una</a>
+        <a href="/olvide" class="acciones__enlace">¿Olvidaste tu Password?</a>
+    </div>
+</main>
